4fa8d69f4d99717783f9543dac3f707f484d81a3
olaniyanp11 <olaniyanp11@.com>
2025-02-26 12:26:01 +0100

    updating the front page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,44 @@
-// import Image from "next/image";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <div className="flex justify-center items-center w-full h-screen">
-        <img src='/bg.jpg' alt="" className=" w-full h-screen object-cover absolute top-0 right-0" />
-        <img src='/red overl.png' alt="" className="w-full h-screen object-cover absolute  top-0 right-0" />
-        <div className=" w-[200px] relative flex justify-center flex-col gap-4 ">
-          <img src='/logo (1).png' alt="" className="w-full " />
-          <img src='/Loader.png' alt="" className="w-full " />
-        </div>      
+      <div className="flex justify-center items-center w-full h-screen relative">
+        {/* Background Image */}
+        <Image
+          src="/bg.jpg"
+          alt="Background"
+          fill
+          className="object-cover"
+          priority
+        />
+
+        {/* Overlay Image */}
+        <Image
+          src="/red overl.png"
+          alt="Red Overlay"
+          fill
+          className="object-cover"
+          priority
+        />
+
+        {/* Logo and Loader */}
+        <div className="w-[150px] relative flex justify-center flex-col gap-4 z-10">
+          <Image
+            src="/logo (1).png"
+            alt="Logo"
+            width={200}
+            height={100}
+            priority
+          />
+          <Image
+            src="/Loader.png"
+            alt="Loader"
+            width={200}
+            height={100}
+            priority
+          />
+        </div>
       </div>
     </>
   );
